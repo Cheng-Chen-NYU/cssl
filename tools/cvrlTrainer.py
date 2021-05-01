3fa92206b1a89e265189b52a983bf5f7f56a4571
Cheng-Chen-NYU <cc6858@nyu.edu>
2021-05-01 16:02:58 +0800

    simclr

--- tools/cvrlTrainer.py
+++ tools/cvrlTrainer.py
@@ -3,10 +3,101 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class cvrlTrainer():
-	def __init__(self, log_dir, model, train_loader, test_loader, optimizer, scheduler):
+	def __init__(self, log_dir, model, train_loader, memory_loader, test_loader, optimizer, temperature, k):
 		self.log_dir = log_dir
 		self.model = model
 		self.train_loader = train_loader
 		self.test_loader = test_loader
 		self.optimizer = optimizer
-		self.scheduler = scheduler
+		self.temperature = temperature
+		self.k = k
+
+	def train(self, epoch_start, epochs):
+		results = {'train_loss': [], 'test_acc@1': [], 'test_acc@5': []}
+		model = nn.DataParallel(self.model).cuda()
+
+		best_acc = 0.0
+		for epoch in range(epoch_start, epochs + 1):
+			
+			model.train()
+
+			total_loss, total_num, train_bar = 0.0, 0, tqdm(self.train_loader)
+			for pos_1, pos_2, target in train_bar:
+				pos_1, pos_2 = pos_1.cuda(non_blocking=True), pos_2.cuda(non_blocking=True)
+				feature_1, out_1 = model(pos_1)
+				feature_2, out_2 = model(pos_2)
+				# [2*B, D]
+				out = torch.cat([out_1, out_2], dim=0)
+				# [2*B, 2*B]
+				sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
+				mask = (torch.ones_like(sim_matrix) - torch.eye(2 * batch_size, device=sim_matrix.device)).bool()
+				# [2*B, 2*B-1]
+				sim_matrix = sim_matrix.masked_select(mask).view(2 * batch_size, -1)
+
+				# compute loss
+				pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+				# [2*B]
+				pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+				loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+				optimizer.zero_grad()
+				loss.backward()
+				optimizer.step()
+
+				total_num += batch_size
+				total_loss += loss.item() * batch_size
+				train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'.format(epoch, epochs, total_loss / total_num))
+
+			results['train_loss'].append(total_loss / total_num)
+
+			model.eval()
+
+			with torch.no_grad():
+				# generate feature bank
+				for data, _, target in tqdm(memory_loader, desc='Feature extracting'):
+					feature, out = model(data.cuda(non_blocking=True))
+					feature_bank.append(feature)
+				
+				# [D, N]	
+				feature_bank = torch.cat(feature_bank, dim=0).t().contiguous()
+				# [N]
+				feature_labels = torch.tensor(memory_data_loader.dataset.targets, device=feature_bank.device)
+				
+				# loop test data to predict the label by weighted knn search
+				test_bar = tqdm(test_loader)
+				for data, _, target in test_bar:
+					data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
+					feature, out = net(data)
+
+					total_num += data.size(0)
+					# compute cos similarity between each feature vector and feature bank ---> [B, N]
+					sim_matrix = torch.mm(feature, feature_bank)
+					# [B, K]
+					sim_weight, sim_indices = sim_matrix.topk(k=k, dim=-1)
+					# [B, K]
+					sim_labels = torch.gather(feature_labels.expand(data.size(0), -1), dim=-1, index=sim_indices)
+					sim_weight = (sim_weight / temperature).exp()
+
+					# counts for each class
+					one_hot_label = torch.zeros(data.size(0) * k, c, device=sim_labels.device)
+					# [B*K, C]
+					one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
+					# weighted score ---> [B, C]
+					pred_scores = torch.sum(one_hot_label.view(data.size(0), -1, c) * sim_weight.unsqueeze(dim=-1), dim=1)
+
+					pred_labels = pred_scores.argsort(dim=-1, descending=True)
+					total_top1 += torch.sum((pred_labels[:, :1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
+					total_top5 += torch.sum((pred_labels[:, :5] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
+					test_bar.set_description('Test Epoch: [{}/{}] Acc@1:{:.2f}% Acc@5:{:.2f}%'
+												.format(epoch, epochs, total_top1 / total_num * 100, total_top5 / total_num * 100))
+
+			results['test_acc@1'].append(total_top1 / total_num * 100)
+			results['test_acc@5'].append(total_top5 / total_num * 100)
+
+			self.scheduler.step()
+
+			data_frame = pd.DataFrame(data=results, index=range(epoch_start, epoch + 1))
+			data_frame.to_csv(self.log_dir + '/log.csv', index_label='epoch')
+			
+			if test_acc_1 > best_acc:
+				best_acc = test_acc_1
+				torch.save({'epoch': epoch, 'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict(), 'scheduler': scheduler.state_dict()}, self.log_dir + '/model_last.pth')
--- train_model.py
+++ train_model.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import json
 import argparse
 import numpy as np
@@ -8,20 +7,26 @@ from datetime import datetime
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
 
-from tools.cvrlDataset import cvrlDataset
+from tools.cvrlDataset import CIFAR10Pair, train_transform, test_transform
 from tools.cvrlTrainer import cvrlTrainer
 from model.model import MoCov1, MoCov2, SimCLRv1, SimCLRv2
 
 parser = argparse.ArgumentParser(description='Contrastive Visual Representation Learning')
-parser.add_argument("-name", "--name", type=str, help="name of the experiment", default="train_mocov1_on_cifar10")
+parser.add_argument("-name", "--name", type=str, help="name of the experiment", default="train_log")
 parser.add_argument("-bs", "--batch_size", type=int, help="batch size", default=512)
-parser.add_argument("-lr", type=float, help="learning rate", default=1e-3)
-parser.add_argument("-momentum", type=float, help="momentum", default=0.9)
-parser.add_argument("-weight_decay", type=float, help="weight decay", default=1e-4)
+parser.add_argument("-t", "--temperature", default=0.5, type=float, help='Temperature used in softmax')
+parser.add_argument("-lr", "--learning_rate", type=float, help="learning rate", default=1e-3)
+parser.add_argument("-m", "--momentum", type=float, help="momentum", default=0.9)
+parser.add_argument("-wd", type=float, help="weight decay", default=1e-6)
 parser.add_argument("-step_size", type=float, help="decay lr every step epochs", default=10)
 parser.add_argument("-gamma", type=float, help="lr decay factor", default=0.5)
-parser.add_argument("-arch", type=str, help="backbone cnn architecture name", default="resnet")
+parser.add_argument("-arch", type=str, help="backbone cnn arch name", default="resnet18")
+parser.add_argument("-model", type=str, help="model name", default="mocov1")
+
+# KNN
+parser.add_argument('-k', default=200, type=int, help='Top k most similar images used to predict the label')
 
 # data
 parser.add_argument("-data_path", type=str, default="data/")
@@ -30,38 +35,47 @@ parser.add_argument("-data_path", type=str, default="data/")
 parser.add_argument("-results_dir", type=str, help="path to cache (default: none)", default='')
 parser.add_argument("-resume", type=str, default='')
 
-def create_folder(log_dir):
-# make summary folder
-	if not os.path.exists(log_dir):
-		os.mkdir(log_dir)
-	else:
-		print('WARNING: summary folder already exists!! It will be overwritten!!')
-		shutil.rmtree(log_dir)
-		os.mkdir(log_dir)
-
 if __name__ == '__main__':
 	args = parser.parse_args()
 
 	if args.results_dir == '':
-		args.results_dir = '/cache-' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S-{}".format(args.name.split('_')[1]))
+		args.results_dir = '/cache-' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S-{}".format(args.arch))
 
-	log_dir = args.name + args.results_dir
-	create_folder(log_dir)
-	config_f = open(os.path.join(log_dir, 'config.json'), 'w')
-	json.dump(vars(args), config_f)
-	config_f.close()
+	if not os.path.exists(args.name):
+		os.mkdir(args.name)
 
-	model_name = args.name.split('_')[1]
+	model_name = args.arch
 	if model_name == 'mocov1':
 		model = MoCov1()
 	elif model_name == 'mocov2':
 		model = MoCov2()
 	elif model_name == 'simclrv1': 
-		model = SimCLRv1(arch='resnet18')
+		model = SimCLRv1(arch=args.arch)
 	elif model_name == 'simclrv2':
-		model = SimCLRv2(arch='resnet18')
+		model = SimCLRv2(arch=args.arch)
 	else:
 		assert(False)
 
-	print('success')
-	#
+	train_data = CIFAR10Pair(root=args.data_path, train=True, transform=train_transform, download=True)
+	train_iter = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=2, pin_memory=True, drop_last=True)
+
+	memory_data = CIFAR10(root=args.data_path, train=True, transform=test_transform, download=True)
+	memory_iter = DataLoader(memory_data, batch_size=args.batch_size, shuffle=False, num_workers=2, pin_memory=True)
+
+	test_data = CIFAR10(root=args.data_path, train=False, transform=test_transform, download=True)
+	test_iter = DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=2, pin_memory=True)
+
+	optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
+
+	log_dir = args.name + args.results_dir
+	if not os.path.exists(log_dir):
+		os.mkdir(log_dir)
+
+	config_f = open(os.path.join(log_dir, 'config.json'), 'w')
+	json.dump(vars(args), config_f)
+	config_f.close()
+
+	trainer = cvrlTrainer(log_dir, model, train_iter, memory_iter, test_iter, optimizer, args.t, args.k)
+	trainer.train(1, 200)
+
+
